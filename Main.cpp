8044ed87ea4e9a2e258e1c5e0f4e96f367debd31
jbreslin33 <jbreslin33@localhost>
2010-11-12 09:19:43 -0500

    ...

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,65 @@
-#include "GameServer.h"
-#include "ListenServer.h"
 
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-int main()
+#define SERVERPORT "4950"	// the port users will be connecting to
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Launching GameServer..." << std::endl;
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
 
-    GameServer*   gameServer   = new GameServer  ();
-    ListenServer* listenServer = new ListenServer();
+	if (argc != 3) {
+		fprintf(stderr,"usage: talker hostname message\n");
+		exit(1);
+	}
 
-    listenServer->setGameServer(gameServer);
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
 
+	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
 
-    bool gameOn = true;
-    while (gameOn == true)
-    {
-        listenServer->processRequests();
-    }
+	// loop through all the results and make a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("talker: socket");
+			continue;
+		}
 
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to bind socket\n");
+		return 2;
+	}
+
+	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+			 p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("talker: sendto");
+		exit(1);
+	}
+
+	freeaddrinfo(servinfo);
+
+	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+	close(sockfd);
+
+	return 0;
 }
+
