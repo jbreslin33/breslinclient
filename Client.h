7fc988e62c7c05fd6a9af585c81cecb4d73897f9
jbreslin33 <jbreslin33@localhost>
2010-11-15 12:18:02 -0500

    ...

---
+++ Client.h
@@ -0,0 +1,29 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    Client.h
+*/
+#ifndef __Client_h_
+#define __Client_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+class Client;
+
+class Client
+{
+
+public:
+    Client();
+    virtual ~Client(void);
+
+
+protected:
+
+};
+
+#endif
+
+
